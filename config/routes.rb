fbad226e22c16366a7cf18d621c1aab5b6bbf3fd
JoeMugwaneza <muganezajoseph@gmail.com>
2017-10-29 22:48:45 +0200

    ability to signed in using id_number or email

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-
   root to: 'pages#welcome'
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   devise_scope :user do
     get 'signout', to: 'devise/sessions#destroy', as: :signout
   end
 end
+
+
