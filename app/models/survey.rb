7f99451ce9c88aef9e4c62aa915571b413e3a524
JoeMugwaneza <muganezajoseph@gmail.com>
2017-10-30 23:44:36 +0200

    Survey is working though it is not perfect

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,7 @@
 class Survey < ApplicationRecord
   belongs_to :user
   has_many :questions, :dependent => :destroy
+  # has_many :answers, through: :questions
+
+   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
