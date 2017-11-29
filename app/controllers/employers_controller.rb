673ef39fb8a8e806d5678c122030cce0bed340cf
JoeMugwaneza <muganezajoseph@gmail.com>
2017-11-29 18:19:32 +0200

    completed employer request form for both create and update

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,4 +1,6 @@
 class EmployersController < ApplicationController
+  before_action :set_employer, :only => [:show, :edit, :update]
+
   def index
     @employers = Employer.all
   end
@@ -11,15 +13,28 @@ class EmployersController < ApplicationController
     @employer = Employer.new(params_employer)
 
     if @employer.save
-      redirect_to edit_employer_request_path(@employer)
+      redirect_to employer_path(@employer)
     else
       render :new
     end
   end
 
+  def update
+    if @employer.update(params_employer)
+      redirect_to employer_path(@employer)
+    else 
+      render :edit
+    end
+
+  end
+
 private
 
   def params_employer
-    params.require(:employer).permit(:first_name, :last_name, :phone_number, :company_name, :company_website, :available_at, :number_of_interns, :stipend_amount, :chain_value_id, employer_field_ids: [])
+    params.require(:employer).permit(:first_name, :last_name, :phone_number, :company_name, :company_website, :available_at, :number_of_interns, :stipend_amount, :chain_value_id, :summary, :field_of_study_ids => [])
+  end
+
+  def set_employer
+    @employer = Employer.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129061006) do
+ActiveRecord::Schema.define(version: 20171129153216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20171129061006) do
     t.datetime "updated_at", null: false
     t.decimal "stipend_amount", precision: 7, scale: 2
     t.integer "chain_value_id"
+    t.text "summary"
   end
 
   create_table "field_of_studies", force: :cascade do |t|
