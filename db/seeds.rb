bc4d275362621c379a9492fd004873fcaaece1b7
JoeMugwaneza <muganezajoseph@gmail.com>
2017-12-11 14:49:34 +0200

    dumped data to the cloud

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,117 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {email: "m.francois@gmail.com", encrypted_password: "$2a$11$a5okLcVIOPxCUdFzjXsuVuyiKJeODchnYx1Qu.k65XhsN3ga7UIIG", first_name: "Mushakamba", last_name: "Francois", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 8, current_sign_in_at: "2017-12-10 10:23:45", last_sign_in_at: "2017-12-09 18:02:12", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false, id_number: "1199280209775044"},
+  {email: "niyomugabaangel@gmail.com", encrypted_password: "$2a$11$s5SgHKyvj7mytv4XlmC/QO0mr/eg/YuF67PA0sOC9CPApy.mrW9rK", first_name: "Angelique", last_name: "Niyomugaba", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 37, current_sign_in_at: "2017-12-02 10:07:45", last_sign_in_at: "2017-11-21 07:27:35", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false, id_number: "199580209775044"},
+  {email: "muganezajoseph@gmail.com", encrypted_password: "$2a$11$PE9XMRC5E0Lwucxrj8nPj.NflbWluiiiTCzeXDQuvBIvJGcsjvYim", first_name: "Joseph", last_name: "Mugwaneza", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 109, current_sign_in_at: "2017-12-11 11:49:46", last_sign_in_at: "2017-12-11 09:12:05", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: true, id_number: nil},
+  {email: "janvierdusabe@gmail.com", encrypted_password: "$2a$11$p2PvM7uX.B0eXmvSUXM.1.0nOYaYUJT5d//EvrDWoPBNVya6QfSg.", first_name: "Janvier", last_name: "Dusabe", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-12-08 08:26:04", last_sign_in_at: "2017-12-08 08:26:04", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false, id_number: "1199580209775044"},
+  {email: "plantejosh1@gmail.com", encrypted_password: "$2a$11$FwyvADJX5iTwYL6iQKKom.seaAl6CQhK7wlEc/4BpUjqM.XB6ibxC", first_name: "Plante", last_name: "Josh", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 5, current_sign_in_at: "2017-11-30 19:46:10", last_sign_in_at: "2017-11-30 19:45:03", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false, id_number: nil},
+  {email: "jeanpaul@gmail.com", encrypted_password: "$2a$11$q4.mTjfoL8lINdGVPfR/.OoG927Hj1eUEti7/5G7k654o1y6vDKDW", first_name: "Jean Paul", last_name: "Nsengiyumva", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 3, current_sign_in_at: "2017-12-02 10:00:59", last_sign_in_at: "2017-11-20 11:28:55", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false, id_number: "198280209775044"},
+  {email: "support@ryaf.rw", encrypted_password: "$2a$11$67XfGM5Pm4eoxIYTcOimje6coFdfqg7HzasBuxqZ5hdCo0rNQmI9W", first_name: "Joseph", last_name: "Mugwaneza", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 8, current_sign_in_at: "2017-12-10 10:18:09", last_sign_in_at: "2017-12-07 07:49:26", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false, id_number: nil},
+  {email: "annemanzi@gmail.com", encrypted_password: "$2a$11$a2pPIzkMnuGhdC3lOlnaYeR6vGZJALow6DGTZsVFuAbbmVYq98VOq", first_name: "Anne", last_name: "Manzi", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 38, current_sign_in_at: "2017-12-10 10:20:42", last_sign_in_at: "2017-12-10 09:22:54", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", admin: false, id_number: "1199780209775044"}
+])
+Answer.create!([
+  {content: "I am working form Nyamagabe with James", question_id: 5, survey_id: 27},
+  {content: "I learned how to grow crop and take care of them", question_id: 6, survey_id: 27},
+  {content: "I am having issues of transport since my employer is not giving me stipend", question_id: 7, survey_id: 27},
+  {content: "If there is a way you can talk to him that can help", question_id: 8, survey_id: 27},
+  {content: "I wish you can help on how I can get transport so that I can reach that the working place earlier", question_id: 9, survey_id: 27},
+  {content: "I am working from Burera with Jean Luc as my  boss", question_id: 5, survey_id: 26},
+  {content: "I have learned a lot of skills and got experience. I learned how to prepare haverst and also make sure that it is prepared with good quality", question_id: 6, survey_id: 26},
+  {content: "It is a challenge for me to work and go home on food. my boss don't give me anything so I have to go home hungry and tried", question_id: 7, survey_id: 26},
+  {content: "If there is a way I can get help about that I can appreciate because it is reduce the way I participate", question_id: 8, survey_id: 26},
+  {content: "I hope if you can help me with that problem other things will be alright.", question_id: 9, survey_id: 26}
+])
+Application.create!([
+  {phone_number: "0725085564", bank_id: 1, bank_number: "4005008383", is_female: false, internship_placement: false, field_of_study_id: 11, user_id: 10, district_id: 4, sector_id: 4},
+  {phone_number: "0736541325", bank_id: 2, bank_number: "4005416789", is_female: false, internship_placement: true, field_of_study_id: 4, user_id: 7, district_id: 6, sector_id: 5},
+  {phone_number: "0736541325", bank_id: 2, bank_number: "4000997543434", is_female: true, internship_placement: true, field_of_study_id: 10, user_id: 6, district_id: 6, sector_id: 3},
+  {phone_number: "0784448430", bank_id: 1, bank_number: "4005007656453", is_female: false, internship_placement: false, field_of_study_id: 8, user_id: 11, district_id: 6, sector_id: 6},
+  {phone_number: "0733601159", bank_id: 2, bank_number: "400058563358", is_female: true, internship_placement: true, field_of_study_id: 2, user_id: 3, district_id: 9, sector_id: 7}
+])
+Bank.create!([
+  {name: "Bk"},
+  {name: "Equity Bank"},
+  {name: "Access Bank"},
+  {name: "BPR"}
+])
+ChainValue.create!([
+  {name: "Rice"},
+  {name: "Banana"},
+  {name: "Cassava"},
+  {name: "Irish Potatoes"},
+  {name: "Ibinyomoro"},
+  {name: "Urutoki"}
+])
+District.create!([
+  {name: "Karongi"},
+  {name: "Kamonyi"},
+  {name: "Muhanga"},
+  {name: "Kicukiro"},
+  {name: "Nyarugenge"},
+  {name: "Gasabo"},
+  {name: "Rutsiro"},
+  {name: "Nyabihu"},
+  {name: "Musanze"}
+])
+Employer.create!([
+  {first_name: "Joseph", last_name: "Mugwaneza", phone_number: "0725085564", company_name: "Joe Farm", company_website: "http://www.nexpals.com/", available_at: "2017-11-01 15:00:00", number_of_interns: 4, stipend_amount: "10000.0", chain_value_id: 1, summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book", email: "group@nexpals.com", closed: false, district_id: 1, sector_id: 1},
+  {first_name: "Jim", last_name: "Musika", phone_number: "0725085564", company_name: "Jim Ltd", company_website: "http://www.nexpals.com/", available_at: "2017-11-01 15:00:00", number_of_interns: 10, stipend_amount: "10000.0", chain_value_id: 6, summary: "I need 2 interns from Agro-economic and 8 interns from soil science", email: "j.musika@gmail.com", closed: false, district_id: 1, sector_id: 1},
+  {first_name: "Plante", last_name: " Josh", phone_number: "0784448430", company_name: "Nexpals", company_website: "http://www.nexpals.com/", available_at: "2017-12-01 14:30:00", number_of_interns: 5, stipend_amount: "20000.0", chain_value_id: 5, summary: "I need 3 interns from horticulture, 1 intern from crop production  and 1 intern in irrigation and Drainage", email: "nexgroup@nexpals.com", closed: false, district_id: 1, sector_id: 1},
+  {first_name: "Luc", last_name: "Abayo", phone_number: "0736541325", company_name: "E-learning Ltd", company_website: "http://www.e-learning.nexpals.com/", available_at: "2017-12-01 17:15:00", number_of_interns: 2, stipend_amount: "50000.0", chain_value_id: 3, summary: "I need 2 interns asap because I have a lot of work they can help me with. I grant you that they will learn a lot of cool stuff with me.", email: "nexlearning@nexpals.com", closed: true, district_id: 1, sector_id: 1},
+  {first_name: "James ", last_name: "Karangwa", phone_number: "078567898", company_name: "James LTD", company_website: "https://www.nexpals.com", available_at: "2017-12-01 16:00:00", number_of_interns: 5, stipend_amount: "10000.0", chain_value_id: 1, summary: "2 from crop production and 3 from irrigation and drainage ", email: "james@jamesltd.com", closed: false, district_id: 6, sector_id: 3},
+  {first_name: "Rwabugiri", last_name: "Anastasie", phone_number: "0736541325", company_name: "Rwabugiri Ltd", company_website: "http://www.nexpals.com/", available_at: "2017-12-01 16:00:00", number_of_interns: 5, stipend_amount: "20000.0", chain_value_id: 3, summary: " I need to 2 interns from food science to help prepare soil....\r\nand I also need 3 interns from agribusiness. I need also one more accountant to help with management.", email: "anastasie@gmail.com", closed: false, district_id: 1, sector_id: 1}
+])
+EmployerField.create!([
+  {employer_id: 1, field_of_study_id: 1},
+  {employer_id: 1, field_of_study_id: 2},
+  {employer_id: 2, field_of_study_id: 5},
+  {employer_id: 2, field_of_study_id: 9},
+  {employer_id: 3, field_of_study_id: 8},
+  {employer_id: 3, field_of_study_id: 12},
+  {employer_id: 3, field_of_study_id: 13},
+  {employer_id: 4, field_of_study_id: 4},
+  {employer_id: 5, field_of_study_id: 8},
+  {employer_id: 5, field_of_study_id: 12},
+  {employer_id: 6, field_of_study_id: 6},
+  {employer_id: 6, field_of_study_id: 10},
+  {employer_id: 6, field_of_study_id: 8}
+])
+FieldOfStudy.create!([
+  {name: "Agro Forestry"},
+  {name: "Forestry and Nature Conservation"},
+  {name: "Biodiversity"},
+  {name: "Rural Development"},
+  {name: "Soil science"},
+  {name: "Food Science"},
+  {name: "Animal Protection"},
+  {name: "Crop Production"},
+  {name: "Agro-economic"},
+  {name: "Agribusiness"},
+  {name: "Agriculture Mechanization"},
+  {name: "Irrigation and Drainage"},
+  {name: "Horticulture"}
+])
+InternshipUpdate.create!([
+  {starting_at: "2018-01-05 00:00:00", chain_value_id: 3, completed: false, application_id: 2, employer_id: 4},
+  {starting_at: "2018-01-05 00:00:00", chain_value_id: 3, completed: false, application_id: 4, employer_id: 4},
+  {starting_at: "2018-01-01 00:00:00", chain_value_id: 3, completed: false, application_id: 3, employer_id: 6}
+])
+Question.create!([
+  {title: "Where do you work from? (Name of Marshland/Company/ cooperative other institution)"},
+  {title: "What did you learn/ N’ iki wigiye aho wakoreraga?"},
+  {title: "Challenges / Ni izihe mbogamizi mwahuye nazo?"},
+  {title: "Any support?/Ni ubuhe bufasha mwifuza?"},
+  {title: "Other comments"}
+])
+Sector.create!([
+  {name: "Murambi", district_id: 1},
+  {name: "Rubengera", district_id: 1},
+  {name: "Kimironko", district_id: 6},
+  {name: "Remera", district_id: 6},
+  {name: "Niboyi", district_id: 4},
+  {name: "kinyinya", district_id: 6},
+  {name: "Musanze", district_id: 9}
+])
+Survey.create!([
+  {name: "AnneManzi.DecemberReport", user_id: 6},
+  {name: "AnneManzi.DecemberReport", user_id: 6}
+])
