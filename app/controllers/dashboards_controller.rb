6415a60e939325b4821be69d9c70a6bc11144eec
JoeMugwaneza <muganezajoseph@gmail.com>
2017-12-18 15:16:48 +0200

    still working on filter and dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,9 +3,7 @@ class DashboardsController < ApplicationController
 
    def index
     if params[:application] && params[:application][:field_of_study_id].empty? && params[:application][:district_id].empty? && params[:application][:sector_id].empty?
-
       @applicants = Application.all
-
     elsif params[:application]
       @applicants = Application.search(params[:application])
     else
