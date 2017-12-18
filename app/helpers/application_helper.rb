ef8fe0a07a921077a6178d35742b33643bdf73fd
JoeMugwaneza <muganezajoseph@gmail.com>
2017-12-18 13:29:38 +0200

    trying filter selector

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def full_name
     full_name = "#{@user.first_name} #{@user.last_name}"
   end
+
+  def graduated_interns
+   InternshipUpdate.joins(:application).where(completed: true)
+  end
+  
 end
