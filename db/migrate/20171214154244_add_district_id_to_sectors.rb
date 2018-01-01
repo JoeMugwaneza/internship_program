83e57b6a40daf7e32b09adcf60d94395141433d2
JoeMugwaneza <muganezajoseph@gmail.com>
2018-01-01 22:30:45 +0200

    debugging

--- db/migrate/20171214154244_add_district_id_to_sectors.rb
+++ db/migrate/20171214154244_add_district_id_to_sectors.rb
@@ -1,5 +1,5 @@
 class AddDistrictIdToSectors < ActiveRecord::Migration[5.1]
   def change
-    add_column :sectors, :district_id, :integer
+    # add_column :sectors, :district_id, :integer
   end
 end
